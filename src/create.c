ce21a43ec2535bfbd2f0a92e7193880c5b9af674
berlintrabby <83368424+berlintrabby@users.noreply.github.com>
2021-12-19 23:16:18 +0100

    fix unset

--- src/create.c
+++ src/create.c
@@ -6,7 +6,7 @@
 /*   By: mwen <mwen@student.42wolfsburg.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/15 14:09:54 by mwen              #+#    #+#             */
-/*   Updated: 2021/12/19 22:21:13 by mwen             ###   ########.fr       */
+/*   Updated: 2021/12/19 23:11:22 by mwen             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,23 @@
 char	**create_envp(char **envp, t_data *data, char *target)
 {
 	int		i;
+	int		j;
 	char	**dup;
 
 	i = -1;
+	j = -1;
 	dup = (char **)malloc(sizeof(char *) * (data->envp_len + 1));
 	if (dup)
 	{
 		dup[data->envp_len] = NULL;
 		while (envp[++i])
 			if (!target || ft_strncmp(envp[i], target, ft_strlen(target)))
-				dup[i] = ft_strdup(envp[i]);
+				dup[++j] = ft_strdup(envp[i]);
 	}
 	else
 		error(data, "malloc for envp failed", 1);
+	int f = -1;
+	while (dup[++f])
+		printf("%s\n", dup[f]);
 	return (dup);
 }
